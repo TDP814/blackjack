d5c86204a1bc6b5fff5b8a9cc928852a69848b78
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2013-10-29 09:10:32 -0600

    Work in progress

--- Deck.java
+++ Deck.java
@@ -1,9 +1,11 @@
 class Deck {
 	
-	private Card[] deck = new Card[52];
+	private Card[] cards = new Card[52];
 
-	public Deck(Card[] deck){
-		
+	public Deck(Card[] cards){
+		for (i=0; i<cards.length; i++) {
+			cards[i].suit = (i + 1)/;
+		}
 	}
 
 }
